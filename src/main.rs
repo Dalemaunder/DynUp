aa751d72566077b1845fc4fcc9cfa50d1a9c57d5
Dale Maunder <dalemaunder@hotmail.com>
2024-05-14 15:12:00 +1000

    Reply with a 200 if correct request type; Reply with 405 if not

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,27 @@ fn read_config() -> (){
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    //let http_request: Vec<_> = buf_reader
+    //    .lines()
+    //    .map(|result| result.unwrap())
+    //    .take_while(|line| !line.is_empty())
+    //    .collect();
+    //
+    //println!("Request: {:#?}", http_request);
 
-    println!("Request: {:#?}", http_request);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    match request_line.as_str() {
+        "HEAD / HTTP/1.1" => {
+            let response = "HTTP/1.1 200 OK\r\n\r\n";
+            stream.write_all(response.as_bytes()).unwrap();
+        },
+        _ => {
+            let response = "HTTP/1.1 405 No\r\n\r\n";
+            stream.write_all(response.as_bytes()).unwrap();
+        }
+    }
+
+
 }
 
 fn main() {
