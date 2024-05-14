aaed1a92c5dd6ec260106b671623fd30a6283e85
Dale Maunder <dalemaunder@hotmail.com>
2024-05-14 14:08:02 +1000

    Read the incoming request data and print to screen for debugging

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,24 @@
 #![allow(non_snake_case)]
-use std::net::TcpListener;
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
+
+
+fn read_config() -> (){
+    ()
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {:#?}", http_request);
+}
 
 fn main() {
 
@@ -10,9 +29,9 @@ fn main() {
     let listener = TcpListener::bind(socket).unwrap();
 
     for stream in listener.incoming() {
-        let _stream = stream.unwrap();
+        let stream = stream.unwrap();
 
-        println!("Connection established!");
+        handle_connection(stream);
     }
 }
 
