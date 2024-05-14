fcc64b3fb1c5d4b2e6f34d2d19329be94ba7a530
Dale Maunder <dalemaunder@hotmail.com>
2024-05-14 23:07:14 +1000

    Read config file and use config settings. Rename main.rs to server.rs.

--- src/server.rs
+++ src/server.rs
@@ -2,12 +2,49 @@
 use std::{
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
+    process::exit,
 };
+use serde_derive::Deserialize;
 
 
-fn read_config() -> (){
-    ()
+#[derive(Debug, Deserialize)]
+struct Settings {
+    server: Config,
 }
+
+#[derive(Debug, Deserialize)]
+struct Config {
+    address: String,
+    listen_port: String,
+    _data_store: String,
+    _users: String,
+    _lifetime: String
+}
+
+
+fn read_config() -> Settings {
+    let file_path = "/home/dwm/.config/DynUp/config.toml";
+
+    let contents = match std::fs::read_to_string(file_path) {
+        Ok(c) => c,
+        Err(_) => {
+            eprintln!("Could not read file `{}`", file_path);
+            exit(1);
+        }
+    };
+
+    let data: Settings = match toml::from_str(&contents) {
+        Ok(d) => d,
+        Err(_) => {
+
+            eprintln!("Unable to load data from `{}`", file_path);
+            eprintln!("{:#?}", &contents);
+            exit(1);
+        }
+    };
+    data
+}
+
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
@@ -20,6 +57,7 @@ fn handle_connection(mut stream: TcpStream) {
     //println!("Request: {:#?}", http_request);
 
     let request_line = buf_reader.lines().next().unwrap().unwrap();
+
     match request_line.as_str() {
         "HEAD / HTTP/1.1" => {
             let response = "HTTP/1.1 200 OK\r\n\r\n";
@@ -34,12 +72,10 @@ fn handle_connection(mut stream: TcpStream) {
 
 }
 
+
 fn main() {
-
-    let server_address = "127.0.0.1";
-    let server_port = "7878";
-    let socket = format!("{}:{}", server_address, server_port);
-
+    let settings = read_config();
+    let socket = format!("{}:{}", settings.server.address, settings.server.listen_port);
     let listener = TcpListener::bind(socket).unwrap();
 
     for stream in listener.incoming() {
