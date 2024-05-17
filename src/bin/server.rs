a9cd38e6ac33d5e1fe68f26ba1c429ea3ee0a982
Dale Maunder <dalemaunder@hotmail.com>
2024-05-17 10:11:09 +1000

    Added regex parsing of the HTTP request and client IP. Added comments.

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -8,11 +8,15 @@ use serde_derive::Deserialize;
 use regex::Regex;
 
 
+
+// Parent struct. Required by Serde.
 #[derive(Debug, Deserialize)]
 struct Settings {
     server: Config,
 }
 
+// Struct for the config of the server itself.
+// TODO: Add defaults
 #[derive(Debug, Deserialize)]
 struct Config {
     address: String,
@@ -22,6 +26,7 @@ struct Config {
     lifetime: String
 }
 
+// Struct for the Database config. Used for the DB connection, whether it's local or remote.
 #[derive(Debug, Deserialize)]
 struct Database {
     address: String,
@@ -30,9 +35,13 @@ struct Database {
     password: String
 }
 
+
+
 fn read_config() -> Settings {
     let file_path = "./config/server_config.toml";
-
+    
+    // Load the config file into a variable.
+    // TODO: Make the error handling here better.
     let contents = match std::fs::read_to_string(file_path) {
         Ok(c) => c,
         Err(_) => {
@@ -41,6 +50,9 @@ fn read_config() -> Settings {
         }
     };
 
+
+    // Parse the config contents into the relevant structs.
+    // TODO: Make the error handling here better.
     let data: Settings = match toml::from_str(&contents) {
         Ok(d) => d,
         Err(_) => {
@@ -54,12 +66,17 @@ fn read_config() -> Settings {
 }
 
 
-fn handle_connection(mut stream: TcpStream) {
-    let _request_re = Regex::new(r".*\s/(\S*)\s.*").unwrap();
-    let _client_address_re = Regex::new(r".*:\s(.*):.*").unwrap();
+fn handle_hello (client_auth: &str, client_IP: &str) {
+    println!("{}", client_auth);
+    println!("{}", client_IP);
+}
 
+
+fn handle_connection(mut stream: TcpStream, request_re: &regex::Regex, client_address_re: &regex::Regex) {
     let buf_reader = BufReader::new(&mut stream);
 
+    // Load the contents of the request into a vec for easy access.
+    // This entire function can definitely be optimised once the MVP is working.
     let mut request_lines = Vec::new();
     for line in buf_reader.lines() {
         let contents = line.unwrap();
@@ -70,24 +87,33 @@ fn handle_connection(mut stream: TcpStream) {
         }
     }
 
-    //for value in &request_lines {
-    //    println!("{:#?}", value);
-    //}
-
-    //println!("{:#?}", &request_line);
     let request = request_lines[0].as_str();
-    let client_address = request_lines[4].as_str();
+    // Capture 1 should be the HTTP method, capture 2 should be the URI.
+    let request_components = request_re.captures(&request).unwrap();
 
-    match request {
-        "PATCH / HTTP/1.1" => {
-            println!("{:#?}", request);
-            println!("{:#?}", client_address);
+
+    let socket = request_lines[4].as_str();
+    // The only capture should be the client's IP address.
+    let socket_components = client_address_re.captures(&socket).unwrap();
+
+
+    // Match the method type from the connection.
+    match &request_components[1] {
+        "PATCH" => {
+            // TODO: Add error handling here.
+            handle_hello(&request_components[2], &socket_components[1]);
+            // Only respond with the 200 code after the hello handling has passed.
             let response = "HTTP/1.1 200 OK\r\n\r\n";
             stream.write_all(response.as_bytes()).unwrap();
         },
+        "GET" => {
+            // Respond with a 204 code for connection testing purposes.
+            // URI is completely ignored.
+            let response = "HTTP/1.1 204 Empty\r\n\r\n";
+            stream.write_all(response.as_bytes()).unwrap();
+        },
         _ => {
-            println!("{:#?}", request);
-            println!("{:#?}", client_address);
+            // Deny all other request methods.
             let response = "HTTP/1.1 405 No\r\n\r\n";
             stream.write_all(response.as_bytes()).unwrap();
         }
@@ -98,15 +124,22 @@ fn handle_connection(mut stream: TcpStream) {
 
 
 fn main() {
+    // TODO: Add error handling here.
     let settings = read_config();
+
+    // TODO: Add error handling here.
     let socket = format!("{}:{}", settings.server.address, settings.server.listen_port);
-    //let socket = format!("{}:{}", "127.0.0.1", "7878");
     let listener = TcpListener::bind(socket).unwrap();
 
+
+    let request_re = Regex::new(r"(.*)\s/(\S*)\s.*").unwrap();
+    let client_address_re = Regex::new(r".*:\s(.*):.*").unwrap();
+
     for stream in listener.incoming() {
+        // TODO: Add error handling here.
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        handle_connection(stream, &request_re, &client_address_re);
     }
 }
 
